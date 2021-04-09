629cae2c6b456ad9e282a58b9121d37ec076cfae
Marco Melletti <mellotanica@hotmail.it>
2021-04-09 14:50:11 +0200

    cleaning up switch dir before performing link to avoid linking issues

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # link switch prod keys directory to the correct spot depending on the user mapping
-ln -s /opt/switch "$(su-exec $PUID:$PGID bash -c 'cd && pwd')/.switch"
+switchdir="$(su-exec $PUID:$PGID bash -c 'cd && pwd')/.switch"
+rm -rf "$switchdir"
+ln -s /opt/switch "$switchdir"
 
 if [ -z "$1" ]; then
     exec su-exec $PUID:$PGID /opt/extract.sh
